2c108cbebcd3cd6e1cf839d28b56eae0378170c1
Dhaval Patel <121498533+thevicecoder@users.noreply.github.com>
2024-01-08 12:27:06 +0530

    test-Payment
    Test Payment Added in Auto Voltage Stabilizer Page

---
+++ script/rezorpay.js
@@ -0,0 +1,30 @@
+        var options = {
+           key: 'rzp_test_sJpyjaezMQUgYU',
+           amount: 20000, // amount in paise (100 paise = 1 INR)
+           currency: 'INR',
+           name: 'Dhaval Patel',
+           description: 'Product Description',
+           image: 'contact.jpg',
+        //    order_id: '123456', // generate a unique order ID on your server
+           handler: function (response) {
+              // Handle the success response
+              alert('Payment successful! Payment ID: ' + response.razorpay_payment_id);
+           },
+           prefill: {
+              name: 'John Doe',
+              email: 'john@example.com',
+              contact: '9876543210'
+           },
+           notes: {
+              address: 'Your Address'
+           },
+           theme: {
+              color: '#F37254'
+           }
+        };
+     
+        var rzp = new Razorpay(options);
+     
+        document.getElementById('rzp-button').onclick = function () {
+           rzp.open();
+        };
